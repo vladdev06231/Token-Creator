f2e29bfdf8bfa8680e824e8f6edf587748e37503
Vladyslav Lukiianchyn <157253698+vladdev06231@users.noreply.github.com>
2024-06-20 13:29:26 +0300

    Update title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
+        <title>Solana Token Editor</title>
         <meta
           name="description"
-          content="Solana Scaffold"
+          content="Solana Token Editor"
         />
       </Head>
       <HomeView />
